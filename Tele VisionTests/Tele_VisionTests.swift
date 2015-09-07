7c27278b9751b0d2246e7acf2735779b5fe6ba51
Zachary Josephson <Zach@Zacharys-MacBook-Pro.local>
2015-09-07 15:32:09 -0700

    Initial Commit

---
+++ Tele VisionTests/Tele_VisionTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tele_VisionTests.swift
+//  Tele VisionTests
+//
+//  Created by Zachary Josephson on 9/7/15.
+//  Copyright (c) 2015 Capstone. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Tele_VisionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
