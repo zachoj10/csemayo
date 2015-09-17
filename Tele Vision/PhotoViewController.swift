c9188a8cf5fc9289f70e3216fed6c2e28ec7ed14
Zachary Josephson <Zach@Zacharys-MacBook-Pro.local>
2015-09-17 11:23:56 -0700

    Photo/Camera implementation

--- Tele Vision/PhotoViewController.swift
+++ Tele Vision/PhotoViewController.swift
@@ -7,10 +7,11 @@
 //
 
 import UIKit
+import MobileCoreServices
 
 
 
-class PhotoViewController: UIViewController {
+class PhotoViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     var name = ""
 
@@ -31,7 +32,69 @@ class PhotoViewController: UIViewController {
     var imagePicker: UIImagePickerController!
     
     @IBOutlet var imageView: UIImageView!
+    var newMedia: Bool?
+    
+    //Function currently works to pick photo from library, as camera cannot be tested in the simulator
+    //To enable camera, uncomment commented lines and comment out related lines
     @IBAction func takePhoto(sender: UIBarButtonItem) {
+        
+        //if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera){
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary){
+            let imagePicker = UIImagePickerController()
+            
+            imagePicker.delegate = self
+            //imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+            imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            imagePicker.mediaTypes = [kUTTypeImage as NSString]
+            imagePicker.allowsEditing = false
+            
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+            
+            //newMedia = true
+            newMedia = false
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        
+        let mediaType = info[UIImagePickerControllerMediaType] as! String
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+        if mediaType == (kUTTypeImage as! String) {
+            let image = info[UIImagePickerControllerOriginalImage]
+                as! UIImage
+            
+            imageView.image = image
+            
+            if (newMedia == true) {
+                UIImageWriteToSavedPhotosAlbum(image, self,
+                    "image:didFinishSavingWithError:contextInfo:", nil)
+            } else if mediaType == (kUTTypeMovie as! String) {
+                // Code to support video here
+            }
+            
+        }
+    }
+    
+    func image(image: UIImage, didFinishSavingWithError error: NSErrorPointer, contextInfo:UnsafePointer<Void>) {
+        
+        if error != nil {
+            let alert = UIAlertController(title: "Save Failed",
+                message: "Failed to save image",
+                preferredStyle: UIAlertControllerStyle.Alert)
+            
+            let cancelAction = UIAlertAction(title: "OK",
+                style: .Cancel, handler: nil)
+            
+            alert.addAction(cancelAction)
+            self.presentViewController(alert, animated: true,
+                completion: nil)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
     
     /*
